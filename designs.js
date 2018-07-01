d4bed7d08c807b7573936f7b5a0cfd81fd4d4fb4
Simone Raaths <Simone Raaths@tonicdesign.local>
2018-07-01 11:17:08 +0200

    grid creation

--- designs.js
+++ designs.js
@@ -3,8 +3,28 @@
 
 // When size is submitted by the user, call makeGrid()
 
+const submitBtn = $('input[type="submit"]');
+const table = $('#pixelCanvas');
+const colorPicker = $('#colorPicker');
+let colorSelected = document.getElementById('colorPicker').value;
+
+console.log(colorSelected);
+
 function makeGrid() {
+  event.preventDefault();
+  if(table.children().length) {
+    table.children().remove();
+  }
 
-// Your code goes here!
+  let gridHeight = document.getElementById('inputHeight').value;
+  let gridWidth = document.getElementById('inputWidth').value;
 
+  for (i = 0; i < gridHeight; i++) {
+    $('#pixelCanvas').append( '<tr></tr>' );
+    console.log("table row");
+  }
+  for (x = 0; x < gridWidth; x++) {
+    console.log("table column");
+    $('#pixelCanvas > tr').append( '<td>hi</td>');
+  }
 }
